db8d035193b90f400a8fbd5fae7c86cb84681cb2
saharabanu <saharabanungn@gmail.com>
2024-01-08 11:50:47 +0600

    banner  design done

--- src/components/ui/Banner.js
+++ src/components/ui/Banner.js
@@ -1,9 +1,31 @@
+import Link from "next/link";
+import { IoIosArrowForward } from "react-icons/io";
 
 
 const Banner = () => {
     return (
-        <div>
-            <h2>This is banner</h2>
+        <div className="flex justify-center items-center pt-7">
+           <div className="space-y-3">
+            <h2 className="text-red-500 uppercase text-2xl font-semibold text-center">Chicken crisper <span className="border rounded-full h-4 ring-1 ring-red-500">r</span> combos</h2>
+
+            <div>
+                <ul className="flex justify-center items-center capitalize space-x-2">
+                    <li className="text-red-500 hover:text-black">
+                        <Link href="/"> Menu</Link>
+                    </li>
+                    <li className="hover:text-red-600 duration-300 delay-150 transition-all">
+                        <Link href="/"><span className="flex items-center"><IoIosArrowForward />chicken</span></Link>
+                    </li>
+                    <li className="hover:text-red-600 duration-300 delay-150 transition-all">
+                        <Link href="/">crisper</Link>
+                    </li>
+                    <li className="hover:text-red-600 duration-300 delay-150 transition-all">
+                        <Link href="/">combos</Link>
+                    </li>
+                </ul>
+            </div>
+            <p className="font-medium">Find everything from our Big Mouth Burgers, our always sizzling, Full-on-Fajitas and our famous Chicken Crispers</p>
+           </div>
         </div>
     );
 };
