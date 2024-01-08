d4c2506241d4030a988b2f9be220e2adce2fc5fc
saharabanu <saharabanungn@gmail.com>
2024-01-08 11:24:31 +0600

    navbar design done

--- src/components/ui/Navbar.js
+++ src/components/ui/Navbar.js
@@ -1,10 +1,14 @@
-"use client"
+"use client";
 
 import { useState } from "react";
 import Cart from "./Cart";
+import Link from "next/link";
+import { HiLockClosed } from "react-icons/hi";
+import { useSelector } from "react-redux";
 
 const Navbar = () => {
-    const [isCartModalOpen, setIsCartModalOpen] = useState(false);
+  const [isCartModalOpen, setIsCartModalOpen] = useState(false);
+  const {books} = useSelector(state => state.cart)
 
   const openCartModal = () => {
     setIsCartModalOpen(true);
@@ -14,14 +18,51 @@ const Navbar = () => {
     setIsCartModalOpen(false);
   };
 
-    return (
-        <div>
-            <h2>This is navbar</h2>
-            <button onClick={openCartModal}>Cart</button>
+  return (
+    <div>
+      <div className="bg-red-50 w-full">
+        <div className="flex justify-between items-center lg:mx-60 py-3">
+          <div>
+            <ul className="uppercase flex lg:space-x-5  font-semibold">
+              <li className="hover:text-red-600 duration-300 delay-150 transition-all">
+                <Link href="/" className="block">
+                  fruits
+                </Link>
+              </li>
+              <li className="hover:text-red-600 duration-300 delay-150 transition-all">
+                <Link href="/" className="block">
+                  menu
+                </Link>
+              </li>
+              <li className="hover:text-red-600 duration-300 delay-150 transition-all">
+                <Link href="/" className="block">
+                  rewards
+                </Link>
+              </li>
+              <li className="hover:text-red-600 duration-300 delay-150 transition-all">
+                <Link href="/">locations</Link>
+              </li>
+              <li className="hover:text-red-600 duration-300 delay-150 transition-all">
+                <Link href="/">gift cards</Link>
+              </li>
+              <li className="hover:text-red-600 duration-300 delay-150 transition-all">
+                <Link href="/">Log in</Link>
+              </li>
+            </ul>
+          </div>
+          <div className="flex items-center">
+            <button onClick={openCartModal} className="pr-3"><p className="flex">
+            <HiLockClosed  className="text-2xl"/> <sup className="bg-red-600 px-[3px] py-[1px] text-center flex items-center text-white rounded-lg ml-[-10px]">{books?.length}</sup>
+              </p></button>
 
-{isCartModalOpen && <Cart onClose={closeCartModal} />}
+            {isCartModalOpen && <Cart onClose={closeCartModal} />}
+            <button className="bg-red-500 text-white px-4 py-2 rounded-lg ">Order Now</button>
+           
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
